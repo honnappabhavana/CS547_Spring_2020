10298d256028cc1bc0bf1093d0cad0c28a94a72d
bilykdo <dmytro@cityuniversity.edu>
2020-05-01 16:03:55 -0700

    Submission for Module 5

--- ON/DmytroBilyk/MyProject/functions/get-index.js
+++ ON/DmytroBilyk/MyProject/functions/get-index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const fs = require('fs'); // Require the fs library to read files
+const fs = require('fs');
+const Mustache = require('mustache'); // Template library.
+const axios = require('axios');
 
-var html; // Set a variable outside of function in order to reuse
+var html;
 
-// The function that will read content from our html file
 const getHtml = () => {
-  if (html) return html; // If the content has existed, do not read it again
-  // Return a promise
+  if (html) return html;
   return new Promise((resolve, reject) => {
     fs.readFile('static/index.html', 'utf8', (err, data) => {
       if (err) reject(err);
@@ -17,16 +17,22 @@ const getHtml = () => {
   });
 };
 
-// The main Lambda function
-module.exports.handler = async (event, context, callback) => {
-  const htmlcontent = await getHtml(); // Get the content
+const fetchBooks = () => axios.get(process.env.fetch_books_api);
 
+module.exports.handler = async (event, context, callback) => {
+  const htmlcontent = await getHtml();
+  const books = await fetchBooks();
+
+  const returnHtml = Mustache.render(htmlcontent, {
+    books: books.data.Items,
+    searchAPI: process.env.search_books_api,
+  });
   const response = {
     statusCode: 200,
-    headers: { // Set up the header
+    headers: {
       'Content-Type': 'text/html; charset=UTF-8',
     },
-    body: htmlcontent,
+    body: returnHtml,
   };
   callback(null, response);
 };
